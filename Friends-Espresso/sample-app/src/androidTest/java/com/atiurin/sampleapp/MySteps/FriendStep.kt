c9778c1139c631ce8ad424121b8a5992c060f772
user1 <lashaxumara@gmail.com>
2023-07-21 22:50:29 +0400

    add friends-homework-multiple-devices

--- Friends-Espresso/sample-app/src/androidTest/java/com/atiurin/sampleapp/MySteps/FriendStep.kt
+++ Friends-Espresso/sample-app/src/androidTest/java/com/atiurin/sampleapp/MySteps/FriendStep.kt
@@ -2,9 +2,14 @@ package com.atiurin.sampleapp.MySteps
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeDown
 import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import com.atiurin.sampleapp.Data.FriendsData
 import com.atiurin.sampleapp.MyPages.FriendPage
+import com.atiurin.sampleapp.helper.isViewDisplayed
+import com.atiurin.sampleapp.helper.scrollSlowlyDown
+import com.atiurin.sampleapp.helper.swipeToBottom
+import com.atiurin.sampleapp.helper.swiper
 import com.atiurin.sampleapp.helper.tap
 import com.atiurin.sampleapp.helper.typeText
 import com.atiurin.ultron.extensions.click
@@ -29,6 +34,48 @@ object FriendStep {
             toolbar.isDisplayed()
             onView(withContentDescription("Open navigation drawer")).perform(click())
             UIEls.click()
+        }
+    }
+
+    // ..........................................new homework steps for Emmet
+    fun swipeUntilEmmet() {
+        var i = 1
+        while (!FriendPage.pickFriendToChat(FriendsData.friendNameEmmet)
+                .isViewDisplayed() && i <= 5
+        ) {
+            swipeToBottom1()
+            i++
+        }
+        FriendPage.pickFriendToChat(FriendsData.friendNameEmmet).click()
+    }
+
+    fun sendTextToEmmet() {
+        FriendPage.messageInput.typeText(FriendsData.textForEmmet)
+        FriendPage.sendButton.click()
+    }
+
+    private fun swipeToBottom1() {
+        with(FriendsData) {
+            swiper(start, end, deley)
+        }
+    }
+
+    fun swipeUntilFriend17() {
+        var b = 1
+        while (!FriendPage.pickFriendToChat(FriendsData.friendName17).isViewDisplayed() && b <= 5) {
+            swipeToBottom2()
+            b++
+        }
+        FriendPage.pickFriendToChat(FriendsData.friendName17).click()
+    }
+
+    fun sendTextToFriend17() {
+        FriendPage.messageInput.typeText(FriendsData.textForFriend17)
+        FriendPage.sendButton.click()
+    }
+    private fun swipeToBottom2() {
+        with(FriendsData) {
+            swiper(start2, end2, deley2)
         }
     }
 }
--- Friends-Espresso/sample-app/src/androidTest/java/com/atiurin/sampleapp/tests/espresso/ChattingTests.kt
+++ Friends-Espresso/sample-app/src/androidTest/java/com/atiurin/sampleapp/tests/espresso/ChattingTests.kt
@@ -3,12 +3,12 @@ package com.atiurin.sampleapp.tests.espresso
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.atiurin.sampleapp.Data.FriendsData
+import com.atiurin.sampleapp.MyPages.FriendPage
+import com.atiurin.sampleapp.MySteps.FriendStep
 import com.atiurin.sampleapp.activity.MainActivity
-import com.atiurin.sampleapp.helper.isTextOnScreen
 import com.atiurin.sampleapp.helper.isViewDisplayed
-import com.atiurin.sampleapp.helper.typeText
-import com.atiurin.sampleapp.pages.UIElementPage
-import com.atiurin.ultron.extensions.tap
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,15 +21,22 @@ class ChattingTests {
     val activityTestRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun textWithMyFriend() {
-        with(UIElementPage) {
-            isTextOnScreen("Friends")
-            nameChandler.isViewDisplayed()
-            nameChandler.tap()
-            textInput.typeText("Hello Rachel")
+    fun textWithMyFriendEmmet() {
+        with(FriendStep) {
+            swipeUntilEmmet()
+            Assert.assertTrue(FriendPage.pickedFriendValidation(FriendsData.friendNameEmmet).isViewDisplayed())
+            sendTextToEmmet()
+            Assert.assertTrue(FriendPage.sentMessage(FriendsData.textForEmmet).isViewDisplayed())
+        }
+    }
 
-            messageInputText.isViewDisplayed()
-
+    @Test
+    fun textWithFriend17() {
+        with(FriendStep) {
+            swipeUntilFriend17()
+            Assert.assertTrue(FriendPage.pickedFriendValidation(FriendsData.friendName17).isViewDisplayed())
+            sendTextToFriend17()
+            Assert.assertTrue(FriendPage.sentMessage(FriendsData.textForFriend17).isViewDisplayed())
         }
     }
 }
